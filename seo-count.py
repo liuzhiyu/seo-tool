63bbcc2057d6e541bbc73cd2356976a7a615a62d
liuzhiyu <liuzhiyu@gmail.com>
2013-03-22 11:48:29 +0800

    查网站收录和目录收录
    查询网站在百度的收录，和其目录的收录

---
+++ seo-count.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import urllib2, re, csv, time
+
+def get_baidu_records_count(host):
+	url = 'http://www.baidu.com/s?wd=site%3A' + host
+	try:
+		data  = urllib2.urlopen(url)
+		html = data.read()
+		soup = BeautifulSoup(html)
+		sitetip = soup.find('p',{'class':'site_tip'})
+		if sitetip.is_empty_element == False:
+			strong = sitetip.find('strong')
+			text = strong.string
+			text = text.replace(',','')
+			numPattern = re.compile(r'\d+')
+			m = numPattern.search(text)
+			strCn = m.group(0)
+			return int(strCn)
+		else:
+			return 0
+	except:
+		return 'error'
+
+
+# read website from a txt file and save them as a list
+
+filename1 = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+filename2 = str(filename1) + '.csv'
+writer=csv.writer(open(filename2, 'wb'))
+writer.writerow(['site', 'count'])
+for site in open("site.txt"):
+	site =  site.rstrip()
+	count = get_baidu_records_count(site)
+	writer.writerow([site, count])
+
+	
---
+++ seo-inurl.py
@@ -0,0 +1,43 @@
+from bs4 import BeautifulSoup
+import urllib2, re, csv, time
+
+def get_baidu_records_count(inurl):
+	host = 'lietou.com'
+	url = 'http://www.baidu.com/s?wd=site%3A' + host + '+' +'inurl%3A' + inurl
+	try:
+		data  = urllib2.urlopen(url)
+		html = data.read()
+		soup = BeautifulSoup(html)
+		sitetip = soup.find('span',{'class':'nums'})
+		if sitetip.is_empty_element == False:
+			text = sitetip.string
+			comma = re.compile(r',')
+			c = comma.search(text)
+			if c == None:
+				numPattern = re.compile(r'\d+')
+				m = numPattern.search(text)
+				strCn = m.group(0)
+				return int(strCn)
+			else:
+				numPattern = re.compile(r'\d+,\d+')
+				m = numPattern.search(text)
+				strCn = m.group(0)
+				return int(strCn.replace(',',''))			
+		else:
+			return 0
+	except:
+		return 'error'
+
+
+# read website from a txt file and save them as a list
+
+filename1 = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+filename2 = str(filename1) + '-inurl.csv'
+writer=csv.writer(open(filename2, 'wb'))
+writer.writerow(['inurl', 'count'])
+for inurl in open("inurl.txt"):
+	inurl =  inurl.rstrip()
+	count = get_baidu_records_count(inurl)
+	writer.writerow([inurl, count])
+
+	
